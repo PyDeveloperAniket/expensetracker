055b223cf31ac8b3b8213d25338d5c1cdedc52ee
Aniket Wandile <63203988+PyDeveloperAniket@users.noreply.github.com>
2024-09-09 23:06:16 +0530

    done with two seperate folder struxture for beckend and frontend

--- server/app.js
+++ server/app.js
@@ -7,7 +7,7 @@ dotenv.config({ path: ".env" });
 const app = express();
 
 const dbURI = process.env.DATABASE;
-const port = process.env.PORT || 5000;
+const port = 5000;
 
 app.use(express.static("public"));
 app.use(express.json());
